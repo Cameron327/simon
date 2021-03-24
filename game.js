25379b781038afc793dd371aa1ab173fe628e616
Cameron <yeec327@gmail.com>
2021-03-24 13:54:11 -0700

    implemented knowing which color the user picked

--- game.js
+++ game.js
@@ -1,15 +1,29 @@
 // we create an array of colors and at each step, add in a new color to the gamePattern array.
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
-var randomChosenColor = buttonColors[nextSequence()];
 
-// add in the random color
-gamePattern.push(randomChosenColor);
+$(".btn").on("click", function(e) {
+    console.log(e);
+    var userChosenColor = e.target.id;
+    console.log(userChosenColor);
+});
 
+
+
+// The nextSequence function generates a random color to be added into the sequence
 function nextSequence() {
     var randomNumber = Math.floor((Math.random() * 4));
+    var randomChosenColor = buttonColors[randomNumber];
 
-    return randomNumber;
+    // add in the random color
+    gamePattern.push(randomChosenColor);
+
+    // Animate the flash for the color
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    // also play sound when that color is selected
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
 }
 
