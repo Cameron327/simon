b2ceca40788c5cb9c8f96ffc7286fbf92b664ed8
Cameron <yeec327@gmail.com>
2021-03-24 14:00:35 -0700

    Refactored code to make a playSound function and added in sounds when a user clicks a button

--- game.js
+++ game.js
@@ -3,14 +3,17 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
-
+// when calling the click function on the class btn, don't forget to use a period
 $(".btn").on("click", function(e) {
     console.log(e);
     var userChosenColor = e.target.id;
-    console.log(userChosenColor);
+    userClickedPattern.push(userChosenColor);
+
+    // also want to play a sound when they click something
+    playSound(userChosenColor);
 });
 
-
+nextSequence();
 
 // The nextSequence function generates a random color to be added into the sequence
 function nextSequence() {
@@ -23,7 +26,12 @@ function nextSequence() {
     // Animate the flash for the color
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
     // also play sound when that color is selected
-    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
-    audio.play();
+    playSound(randomChosenColor);
+    
 }
 
+// refactored code to make a play sound function since I use it twice
+function playSound(color) {
+    var audio = new Audio("sounds/" + color + ".mp3");
+    audio.play();
+}
